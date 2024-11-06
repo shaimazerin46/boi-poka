7f83804ccef1d70a8311bdd40e6d05455288f137
unknown <shaimazerin46@gmail.com>
2024-11-06 12:20:37 +0600

    navbar done

---
+++ src/components/Root/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="w-4/5 mx-auto">
+            <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="text-xl font-bold">Book Vibe</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a href="">Listed Books</a></li>
+      <li><a>Pages to Read</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end space-x-3 font-semibold">
+    <a className="btn bg-[#23BE0A] text-white ">Sign In</a>
+    <a className="btn bg-[#59C6D2] text-white ">Sign Up</a>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Navbar;
